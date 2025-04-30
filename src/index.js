666d8ec74dea885557e56610a1821a729bc7c604
Jonathan Sedrez <jonathansedrez@gmail.com>
2025-04-30 10:03:49 -0300

    feat: add health check

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-const crypto = require('crypto');
+const crypto = require("crypto");
 
 console.log("Bun http running on http://127.0.0.1:3000/");
 Bun.serve({
   fetch(req) {
     const url = new URL(req.url);
-    
-    if (url.pathname === '/') {
+
+    if (url.pathname === "/") {
       const uuid = crypto.randomUUID();
-      return new Response(uuid, { status: 200, headers: { 'Content-Type': 'text/plain' } });
+      return new Response(uuid, {
+        status: 200,
+        headers: { "Content-Type": "text/plain" },
+      });
     }
-    
-    return new Response('404!', { status: 404, headers: { 'Content-Type': 'text/plain' } });
+
+    return new Response("404!", {
+      status: 404,
+      headers: { "Content-Type": "text/plain" },
+    });
+  },
+  routes: {
+    "/health": () => {
+      return new Response("ok!");
+    },
   },
 });
